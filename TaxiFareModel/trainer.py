b0a5e72c893a877641a75e6d2193cac7586e5a65
Sergio Ponce <sergio.poncet@gmail.com>
2021-06-24 11:24:38 +0200

    ML integration

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,6 +1,16 @@
 # imports
+from memoized_property import memoized_property
+import mlflow
+import joblib
+from sklearn.pipeline import Pipeline
+
 
 class Trainer():
+    
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+    
+    EXPERIMENT_NAME = "[FR][Lyon][SergioPx] TaxiFareNY 1.0"
+    
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -12,6 +22,8 @@ class Trainer():
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
+        pipeline = Pipeline(steps=[('scaler', StandardScaler()),
+                           ('regressor', LinearRegression())])
         pass
 
     def run(self):
@@ -21,6 +33,42 @@ class Trainer():
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         pass
+    
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self.pipeline, 'model.joblib')
+    
+        return
+    
+    #...
+    
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)    
+    
+    
+    
+    
+    
+    
 
 
 if __name__ == "__main__":
